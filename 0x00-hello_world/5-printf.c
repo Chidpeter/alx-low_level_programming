1a78bf8b1fad2011ce13bb01f80630a7a526992f
chidpeter <chidipeteramadi@gmail.com>
2023-07-15 07:01:07 -0700

    Project on C Hello

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[] = "with proper grammar, but the outcome is a piece of art,";
+
+	printf("%s", a);
+	return (0);
+}

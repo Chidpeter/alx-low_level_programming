9631074c2ad4bf64c890f1c1a32391c2c14cc585
chidpeter <chidpeter@Larryxcell>
2023-07-16 16:10:17 +0100

    My personal work

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Using putchar function to print lowercase alphabets
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz"
+
+	putchar("a\n");
+	return (0);
+}

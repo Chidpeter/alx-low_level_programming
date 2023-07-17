d7abc0bb39317f943873125de4cc6d557bf58661
chidpeter <chidpeter@Larryxcell>
2023-07-17 04:22:51 +0100

    My personal work

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints single numbers with commas and spaces
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; ++n)
+	{
+	putchar(n);
+	putchar(',');
+	putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}

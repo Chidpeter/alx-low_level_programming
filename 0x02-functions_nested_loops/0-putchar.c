8226238b06a61602c99daf2470a4dda56f72a207
chidpeter <chidpeter@Larryxcell>
2023-07-19 18:21:51 +0100

    My personal work

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,8 @@
  * Return: Always 0 (Success)
  */
 
-int main(void);
+int main(void)
 {
-	char _putchar = a
-
 	_putchar('_');
 	_putchar('p');
 	_putchar('u');
@@ -19,8 +17,6 @@ int main(void);
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-	{
 	_putchar('\n');
-	}
 	return (0);
 }

d62de590804a5d994b9249b660ab890c976b9543
chidpeter <chidpeter@Larryxcell>
2023-07-21 22:47:19 +0100

    Project on C debugging

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - A function that print numbers
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0; a <= 9; a++)
+	{
+	_putchar(a);
+	}
+	_putchar('\n');
+	return (a);
+}

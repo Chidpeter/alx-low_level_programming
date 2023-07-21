050f990838d3f360c9e23d305f2d4fa16fc9a85c
chidpeter <chidpeter@Larryxcell>
2023-07-21 23:19:16 +0100

    Project on C debugging

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Print numbers without 2 and 4
+ */
+
+void print_most_numbers(void)
+{
+	int a;
+
+	for (a = 48; a < 58; a++)
+	{
+	if (a == 50 || a == 50)
+	{
+	continue;
+	}
+	_putchar(a);
+	}
+	_putchar('\n');
+}

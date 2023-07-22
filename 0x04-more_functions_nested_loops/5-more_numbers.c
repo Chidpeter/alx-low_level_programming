2a6a0275532e339d4844844102169a56f7e8ecaf
chidpeter <chidpeter@Larryxcell>
2023-07-22 02:53:49 +0100

    Project on C debugging

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints numbers
+ * Return: Always 0
+ */
+
+int more_numbers(void)
+{
+	int a;
+	int b;
+
+	for (b = 0; b <= 10; b++)
+	{
+	for (a = 0; a <= 14; a++)
+	{
+	if (a > 9)
+	_putchar(a / 10 + '0');
+	_putchar(a % 10 + '0');
+	}
+	_putchar('\n');
+	}
+}

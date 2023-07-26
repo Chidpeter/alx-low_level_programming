5141e52dcceb7125d04362cdd4615d7924e99016
chidpeter <chidpeter@Larryxcell>
2023-07-26 03:13:52 +0100

    Project on C pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * puts_half - A function that prints half of the string
+ * @str: string to be printed
+ * Return: the second half of the string
+ */
+
+void puts_half(char *str)
+{
+	int a, b, c;
+
+	c = 0;
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	c++;
+	b = (c / 2);
+	if ((c % 2) == 1)
+	{
+	n = ((c + 1) / 2);
+	for (a = b; str[a] != '\0'; a++)
+	_putchar(str[a]);
+	}
+	_putchar('\n');
+	}
+}

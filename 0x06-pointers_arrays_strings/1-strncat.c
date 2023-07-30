9aeaee49760f5e4aaddaa353d0b9d13b2f75ae72
chidpeter <chidpeter@Larryxcell>
2023-07-30 21:06:49 +0100

    More project on pointers, strings and arrays

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -6,7 +6,7 @@
  * @src: input value
  * @n: input value
  *
- * Return: dast
+ * Return: dest
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -17,7 +17,7 @@ char *_strncat(char *dest, char *src, int n)
 	i = 0;
 	while (dest[i] != '\0')
 	{
-	i++
+	i++;
 	}
 	j = 0;
 	while (j < n && src[j] != '\0')

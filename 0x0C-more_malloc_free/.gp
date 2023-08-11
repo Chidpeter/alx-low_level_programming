518719347303827d08142fe2893177e9b480c2f1
chidpeter <chidpeter@Larryxcell>
2023-08-11 02:53:18 +0100

    Project on More Malloc

---
+++ 0x0C-more_malloc_free/.gp
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add .
+git commit -m "Project on More Malloc"
+git push
---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - a function that allocates memory using malloc
+ * @b: size of byte to be allocated
+ *
+ * Return: a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a;
+
+	a = malloc(b);
+	if (a == NULL)
+	exit(98);
+	return (0);
+}

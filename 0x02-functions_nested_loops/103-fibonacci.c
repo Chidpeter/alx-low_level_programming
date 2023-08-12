b2a42514bdd1f698c3f521a95129397735ff2594
chidpeter <chidpeter@Larryxcell>
2023-08-12 05:13:42 +0100

    My personal work

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main - prints even valued numbers of fib num less 4,000,000
+ * main - prints the sum of even valued numbers of fib num less 4,000,000
  *
  * Return: 0
  */
@@ -9,16 +9,19 @@
 int main(void)
 {
 	int c;
-	unsigned int f1 = 0, f2 = 1, sum;
+	unsigned long f1 = 1, f2 = 2, sum = 0, sum1;
 
-	for (c = 0; c < 32; c++)
+	for (c = 0; c <= 33; c++)
 	{
-	sum = f1 + f2;
-	if ((sum % 2) == 0)
-	printf("%d", sum);
-	f1 = f2;
-	f2 = sum;
+	if (f1 < 4000000 && (f1 % 2) == 0)
+	{
+	sum += f1;
 	}
+	sum1 = f1 + f2;
+	f1 = f2;
+	f2 = sum1;
+	}
+	printf("%lu", sum);
 	printf("\n");
 	return (0);
 }

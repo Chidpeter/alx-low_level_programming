2c9e96a5cc0e1076527169d429f518fe45bd0de9
chidpeter <chidpeter@Larryxcell>
2023-09-04 20:59:35 +0100

    Project on file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -2,17 +2,11 @@
 
 
 /**
-
  * create_file - This fn creates a file.
-
  * @filename: A pointer to the file to be created.
-
  * @text_content: A pointer to a string to write to the file.
-
  *
-
  * Return: If the fn fails -1, otherwise return 1.
-
  */
 
 int create_file(const char *filename, char *text_content)
@@ -31,10 +25,9 @@ int create_file(const char *filename, char *text_content)
 	{
 	for (length = 0; text_content[length])
 	length++;
-        }
+	}
 
-
-        chidi = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	chidi = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 
 	writer = write(chidi, text_content, length);
 	if (chidi == -1 || writer == -1)

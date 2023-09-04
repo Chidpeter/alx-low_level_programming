3dd820329317418bdb80991f0b51243b849142f5
chidpeter <chidpeter@Larryxcell>
2023-09-04 14:48:50 +0100

    Project on file i/o

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -16,7 +16,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 	char *buffer;
 
-	chidi = fopen(filename, O_RDWR);
+	chidi = open(filename, O_RDWR);
 	if (chidi == -1)
 	return (0);
 

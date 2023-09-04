a604246ef5c8bdfba7c1bb79c71b3afd46c62fcb
Larryxcell <Larryxcell@gmail.com>
2023-09-04 17:11:34 +0100

    Project on file i/o

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -27,7 +27,8 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content != NULL)
 	{
-	for (length = 0; text_content[length]; length++)
+	for (length = 0; text_content[length])
+	length++;
         }
 
 
